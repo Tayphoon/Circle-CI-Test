b5a1918d644f0684417ef8f2cd6d5035118b390e
Tayphoon <tayphoon.company@gmail.com>
2018-05-31 11:56:21 +0300

    Init commit

---
+++ CI-config/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CI-config
+//
+//  Created by Tayphoon on 10/12/2017.
+//  Copyright © 2017 Inostudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+use_frameworks!
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
+
+target 'CI-config' do
+
+end
+
+target 'CI-configTests' do
+    
+end
